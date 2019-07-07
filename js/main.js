af5024c49793d5cf467d39187d236ea5fbc8cc39
Droideka <vindou@mail.ru>
2019-07-07 22:56:56 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let block = document.querySelector('.block');
+let circle = document.querySelector('.progress__circle'),
+       radius = circle.r.baseVal.value,
+       length = 2 * Math.PI * radius;
+   circle.style.strokeDasharray = `${length} ${length}`;
+   circle.style.strokeDashoffset = length;
+block.addEventListener('click', (e) => {
+   block.style.pointerEvents = 'none';
+   let width = 0;
+   let handler = setInterval(() => { 
+      let offset = length - width / 100 * length;
+      circle.style.strokeDashoffset = offset; 
+      if (width > 99.9) {
+         clearInterval(handler);
+         block.style.pointerEvents = "auto"
+      } else {
+         width += 0.1;
+         +width.toFixed(1);
+      }   
+   },5);
+})
